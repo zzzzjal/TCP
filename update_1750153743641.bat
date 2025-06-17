674d14248d9cd88c280fa70e82e81832db2cbfa2
zzzzjal <117327539+zzzzjal@users.noreply.github.com>
2025-06-17 20:28:54 +0800

    test1

--- update_1750153743641.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-timeout /t 3 >nul
-del "D:\TCP\TCP\target\classes"
-move /Y "C:\Users\16563\AppData\Local\Temp\TCP-update1196438347695800423\TCP-new.jar" "D:\TCP\TCP\target\classes"
-start "" "D:\TCP\TCP\target\classes"
-rmdir /s /q "C:\Users\16563\AppData\Local\Temp\TCP-update1196438347695800423"
-del "%~f0"
